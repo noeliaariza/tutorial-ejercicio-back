184c92049b566ee0b54945cfbca58c856d71a628
Ariza Lechuga external <maria-noelia.ariza-lechuga.external@capgemini.com>
2024-09-27 12:14:41 +0200

    First upload file structure

---
+++ src/test/java/com/ccsw/ejclientes/EjclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccsw.ejclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
