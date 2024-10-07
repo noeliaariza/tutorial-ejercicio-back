80348697aec64e359d1675b252b05abb5a7b6b7a
Ariza Lechuga external <maria-noelia.ariza-lechuga.external@capgemini.com>
2024-10-07 13:52:17 +0200

    -

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,3 @@
-INSERT INTO clients(name) VALUES ('Pepe Botella');
-INSERT INTO clients(name) VALUES ('Jesucristo García');
-INSERT INTO clients(name) VALUES ('Don Pimpón');
-
 INSERT INTO category(name) VALUES ('Eurogames');
 INSERT INTO category(name) VALUES ('Ameritrash');
 INSERT INTO category(name) VALUES ('Familiar');
